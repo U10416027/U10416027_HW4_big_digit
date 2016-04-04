a95026030b0de83defcada1cdfa1d843cea70ecd
U10416027 <U10416027@go.utaipei.edu.tw>
2016-04-04 21:36:00 +0800

    Update BigDigit.java
    get the two big number

--- BigDigit.java
+++ BigDigit.java
@@ -10,6 +10,17 @@ public class BigDigit{
 		this.num2 = bignum2;
 	}
 	
+	public void setFitstnum(){
+		for(int i = 0; i < bignum1.length(); i++){
+			firstnum.add(bignum1.charAt(i));
+		}
+	}
+	
+	public void setSecondnum(){
+		for(int i = 0; i < bignum2.lengh(); i++){
+			secondnum.add(bignum2.charAt(i));
+		}
+	}
 	
 	
 	
