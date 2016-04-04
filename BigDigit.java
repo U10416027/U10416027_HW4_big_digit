efdf1832903895b81fb450f7d72a80699265c1d1
U10416027 <U10416027@go.utaipei.edu.tw>
2016-04-04 21:24:35 +0800

    Update BigDigit.java

--- BigDigit.java
+++ BigDigit.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class BigDigit{
-	
-	
-	
+	private ArrayList<Integer> num1 = new ArrayList<>();
+	private ArrayList<Integer> num2 = new ArrayList<>();
+	private ArrayList<Integer> numplus = new ArrayList<>();
 	
 	
 	
