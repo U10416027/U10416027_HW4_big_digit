6b536b1bee8b74664332e13efedecb9f8f11b413
U10416027 <U10416027@go.utaipei.edu.tw>
2016-04-04 15:07:29 +0800

    Update UseBigDigit.java

--- UseBigDigit.java
+++ UseBigDigit.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class UseBigDigit{
+	public static void main(string[] args){
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+  }
+}
