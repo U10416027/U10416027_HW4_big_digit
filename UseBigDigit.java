72f9e979f648082e08b5db2b6e2ec0b069b9c645
U10416027 <U10416027@go.utaipei.edu.tw>
2016-04-04 15:15:15 +0800

    Update UseBigDigit.java

--- UseBigDigit.java
+++ UseBigDigit.java
@@ -4,6 +4,8 @@ public class UseBigDigit{
 	public static void main(string[] args){
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("輸入兩個要加起來的大數");
+		
 		String num1 = input.nextLine();
 		String num2 = input.nextLine();
 		
